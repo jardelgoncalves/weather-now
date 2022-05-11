b2175c55df027ee9a9e57e88366726e5cc21b7eb
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-05-11 02:50:33 -0300

    chore: create layout template

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -31,4 +31,37 @@ module.exports = (plop) => {
       },
     ],
   })
+
+  plop.setGenerator('layout', {
+    description: 'Create a layout',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your layout name?',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/layout/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/layout.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/components/layout/{{pascalCase name}}/styles.module.scss',
+        templateFile: 'templates/styles.scss.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/components/layout/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+        templateFile: 'templates/stories.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/components/layout/{{pascalCase name}}/{{pascalCase name}}.spec.tsx',
+        templateFile: 'templates/test.tsx.hbs',
+      },
+    ],
+  })
 }
